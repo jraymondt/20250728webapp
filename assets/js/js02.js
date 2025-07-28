77645b68b18cf087616a4ba417ae06426fa264dc
jraymondt <james@jraymondt.com>
2025-07-28 14:07:06 +0800

    not working

---
+++ assets/js/js02.js
@@ -0,0 +1,13 @@
+// Challenge 2 Starter
+// Initialise the arrays
+let array1 = [1, 2, 3, 2];
+let array2 = [4, 5, 6, 4];
+
+// Use the spread operator to merge the two arrays
+[...mergedArray] = [...array1, ...array2];
+// Use the Set object to remove duplicates
+unitArray = [...new Set(mergedArray)];
+// Use array destructuring to assign the first three elements to variables
+array3 = [1, 2, 3];
+// Set the value of the output filed to be the second element of the unique array
+
